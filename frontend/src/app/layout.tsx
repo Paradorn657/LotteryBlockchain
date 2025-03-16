6f1e225ccad9e221d75d646760fedb2bda6724c8
Thitiphatx <thitiphat6391@gmail.com>
2025-03-16 16:12:55 +0700

    poon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
                 className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
                 <li><a>Item 1</a></li>
                 <li>
-                  <a>Parent</a>
+                  <a>Kuy</a>
                   <ul className="p-2">
                     <li><a>Submenu 1</a></li>
                     <li><a>Submenu 2</a></li>
